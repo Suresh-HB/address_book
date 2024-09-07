3a1d789f014226826a04b779d7e647ad8ac557d8
Suresh HB <sureshsai175475@gmail.com>
2024-09-07 20:40:32 +0530

    [Suresh] Add: Adding contact into addressBook

--- addressBook.py
+++ addressBook.py
@@ -1,14 +1,3 @@
-"""
-
-@Author: Suresh
-@Date: 07-09-2024
-@Last Modified by: Suresh
-@Last Modified Date:07-09-2024
-@Title : Addressbook.
-
-"""
-
-
 class Contact:
     
     def __init__(self, first_name, last_name, address, city, state, zip_code, phone_number, email):
@@ -21,15 +10,45 @@ class Contact:
         self.phone_number = phone_number
         self.email = email
 
+    def __str__(self):
+        """
+        Description: Function for returning the actual data as per requirement.
+        Parameters : 
+           None:
+        Return: returning a string representation of the contact.
+        """
+        return (f"Name: {self.first_name} {self.last_name}\n"
+                f"Address: {self.address}, {self.city}, {self.state} {self.zip_code}\n"
+                f"Phone: {self.phone_number}\n"
+                f"Email: {self.email}")
 
 class AddressBook:
 
     def __init__(self):
         self.address_book = []
 
+    def add_contact(self, contact):
+        self.address_book.append(contact)
 
 def main():
     ab = AddressBook()
+
+    print("Enter contact details:")
+    first_name = input("First Name: ")
+    last_name = input("Last Name: ")
+    address = input("Address: ")
+    city = input("City: ")
+    state = input("State: ")
+    zip_code = input("Zip Code: ")
+    phone_number = input("Phone Number: ")
+    email = input("Email: ")
+
+    c = Contact(first_name, last_name, address, city, state, zip_code, phone_number, email)
+    ab.add_contact(c)
+
+    print("\nAddress Book:")
+    for data in ab.address_book:
+        print(data)
 
 if __name__ == "__main__":
     main()
